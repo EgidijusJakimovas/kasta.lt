ee3c2d14bf42769b8280d971729f110aa77b8e21
EgidijusJakimovas <egidijusjakimovas@gmail.com>
2024-08-14 18:12:41 +0300

    updated composer.lock

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '5a6ad99bf37bd39eb5bee4d65a5378cb9cad5c8d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '5a6ad99bf37bd39eb5bee4d65a5378cb9cad5c8d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
