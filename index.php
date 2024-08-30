7d8e73978b51f5f9d749c5016fb147231b27adbc
EgidijusJakimovas <egidijusjakimovas@gmail.com>
2024-08-30 14:02:55 +0300

    updated url logic

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <?php
 
-// Include the content of home.html
-include 'home.html';
+// Get the current path from the URL
+$path = $_SERVER['REQUEST_URI'];
+
+// Check the path and include the corresponding HTML file
+if ($path == '/valymo_irenginiai') {
+    include('wastewater_treatment_systems.html');
+} elseif ($path == '/tvora') {
+    include('fence_installation.html');
+} else {
+    // Include the content of home.html for the root path and other undefined paths
+    include('home.html');
+}
 
 ?>
