99dcfaa8e8a458f13bdb2ad00e2b10855c2e27ed
EgidijusJakimovas <egidijusjakimovas@gmail.com>
2024-08-30 14:21:34 +0300

    updated info

--- index.php
+++ index.php
@@ -1,16 +1,6 @@
 <?php
 
-// Get the current path from the URL
-$path = $_SERVER['REQUEST_URI'];
-
-// Check the path and include the corresponding HTML file
-if ($path == '/valymo_irenginiai') {
-    include('wastewater_treatment_systems.html');
-} elseif ($path == '/tvora') {
-    include('fence_installation.html');
-} else {
-    // Include the content of home.html for the root path and other undefined paths
-    include('home.html');
-}
+// Include the content of home.html
+include 'home.html';
 
 ?>
