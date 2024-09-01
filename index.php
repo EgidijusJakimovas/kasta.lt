459ca2cd9ddce2e1a26d93ce8ee9930031521fa7
EgidijusJakimovas <egidijusjakimovas@gmail.com>
2024-09-01 14:23:36 +0300

    updated link names

--- index.php
+++ index.php
@@ -1,17 +1,21 @@
 <?php
-// Get the current path from the URL, trimming any trailing slashes and query strings
-$path = rtrim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
+// Get the current path from the URL, trimming any trailing slashes
+$path = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
 
 // Debug: Check what path is being captured (optional)
 // echo "Path: $path<br>";
 
-// Check the path and include the corresponding HTML file
-if ($path == '/valymo_irenginiai') {
-    include('wastewater_treatment_systems.html');
-} elseif ($path == '/tvora') {
-    include('fence_installation.html');
-} else {
-    // Include the content of home.html for the root path and other undefined paths
-    include('home.html');
+// Determine which file to include based on the path
+switch ($path) {
+    case 'valymo_irenginiai':
+        include('wastewater_treatment_systems.html');
+        break;
+    case 'tvora':
+        include('fence_installation.html');
+        break;
+    default:
+        // Include home.html for root and undefined paths
+        include('home.html');
+        break;
 }
 ?>
